805881ede5a0e0665a94856d3429f64df3a3e5f4
Caio <caiocpe@gmail.com>
2024-12-05 16:22:10 -0300

    config: config remotePattern to next image src

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+      minimumCacheTTL: 60 * 1 * 60 * 24,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/**',
+        port: '',
+        search: ''
+      },
+    ],
+  },
 };
 
 export default nextConfig;
