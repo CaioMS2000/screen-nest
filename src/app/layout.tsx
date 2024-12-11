592d9f8c21db0a252dfe1bf1585896562eb374c3
Caio <caiocpe@gmail.com>
2024-12-11 20:29:02 -0300

    style: not let empty content make page short

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={'dark bg-app-black antialiased'}>
+			<body className={'dark h-screen bg-app-black antialiased'}>
 				<Providers>{children}</Providers>
 			</body>
 		</html>
