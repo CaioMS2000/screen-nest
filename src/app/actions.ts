fb405fe29294d92b88826b1d92c17578bb7a03c4
Caio <caiocpe@gmail.com>
2024-12-12 04:47:42 -0300

    chore: inheriting Next fetch api extras

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,7 +3,14 @@ import { get } from '@/utils/tmdb'
 import { FetchMoviesResponse, FetchSeriesResponse } from './@types/http'
 
 export async function fetchMovies(query: string): Promise<FetchMoviesResponse> {
-	const response = await get(`/search/movie?query=${query}&language=pt-BR`)
+	const response = await get(`/search/movie?query=${query}&language=pt-BR`, {
+		//  cache: 'force-cache',
+		//  next: {
+		// 	 revalidate: 1 * 60 * 60,
+		// 	 tags: ['movie', `movie-${query}`]
+		//  }
+		// nesse caso em específico o cache está sendo feito por lib externa
+	})
 	const data = await response.json()
 	return {
 		page: data.page,
@@ -14,7 +21,14 @@ export async function fetchMovies(query: string): Promise<FetchMoviesResponse> {
 }
 
 export async function fetchSeries(query: string): Promise<FetchSeriesResponse> {
-	const response = await get(`/search/tv?query=${query}&language=pt-BR`)
+	const response = await get(`/search/tv?query=${query}&language=pt-BR`, {
+		// cache: 'force-cache',
+		// next: {
+		// 	revalidate: 1 * 60 * 60,
+		// 	tags: ['tv', `tv-${query}`]
+		// },
+		// nesse caso em específico o cache está sendo feito por lib externa
+	})
 	const data = await response.json()
 	return {
 		page: data.page,
--- src/utils/tmdb.ts
+++ src/utils/tmdb.ts
@@ -1,25 +1,37 @@
 import { TMDB_BASE_URL } from '@/constants/http'
 import { env } from '@/env'
+import { RequestInit } from 'next/dist/server/web/spec-extension/request'
 
 export type FetchOptions = {
-	method: string
-	headers: {
+	method?: string
+	headers?: {
 		accept: string
 		Authorization: string
 	}
-}
-
+} & RequestInit
+// interface FetchOptions extends RequestInit {
+// 	method?: string
+// 	headers?: {
+// 		accept: string
+// 		Authorization: string
+// 	}
+// }
 const defaultOptions: FetchOptions = {
 	method: 'GET',
 	headers: {
 		accept: 'application/json',
 		Authorization: `Bearer ${env.NEXT_PUBLIC_TMDB_TOKEN}`,
 	},
+	cache: 'force-cache',
 }
 
-export function get(url: string, options: FetchOptions = defaultOptions) {
+export function get(url: string, options: FetchOptions = {} as FetchOptions) {
+	const optionsToUse = {
+		...defaultOptions,
+		...options,
+	}
 	const isUrlComplete = url.startsWith('http')
 	const urlToFetch = isUrlComplete ? url : `${TMDB_BASE_URL}${url}`
 
-	return fetch(urlToFetch, options)
+	return fetch(urlToFetch, optionsToUse)
 }
