5b42d7c4252df2902e8dc3198df7a9a5333a3ee8
Caio <caiocpe@gmail.com>
2024-12-13 10:16:46 -0300

    style: re styling UI

--- src/app/(public)/components/header.tsx
+++ src/app/(public)/components/header.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { Search01Icon } from '@/components/houstonicons/search'
 import ImageComponent from '@/components/image-component'
-import { Button } from '@nextui-org/button'
 import { Input } from '@nextui-org/input'
 import { useRouter } from 'next/navigation'
 import ActionArea from './action-area'
@@ -21,15 +20,21 @@ export default function Header({
 	return (
 		<>
 			<header className="flex items-center justify-between bg-app-black-100 px-4 py-2 text-white">
-				<div className="flex items-center gap-4">
-					<ImageComponent
-						width={0}
-						height={0}
-						sizes="600px"
-						className="w-28"
-						src="/images/video.png"
-						alt="logo"
-					/>
+				<div className="flex items-center gap-5">
+					<div
+						className="flex cursor-pointer items-center gap-2"
+						onClick={() => router.push('/')}
+					>
+						<ImageComponent
+							width={0}
+							height={0}
+							sizes="600px"
+							className="w-16"
+							src="/images/video.png"
+							alt="logo"
+						/>
+						<h3 className="font-bold text-lg">Screen Nest</h3>
+					</div>
 					<div className="flex items-center gap-2">
 						<Input
 							className="w-96 placeholder:font-bold placeholder:text-white"
